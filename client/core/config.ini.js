40431b551cc086a8ecd296f113f3c9d7c9d2a252
kevposesp <kevposesp@gmail.com>
2024-07-20 11:49:31 +0200

    delete config client

---
+++ client/core/config.ini.js
@@ -0,0 +1,4 @@
+const coreConfig = {
+    EXPRESS_URL: 'http://localhost:3000/api/',
+}
+export default coreConfig
--- client/core/config.js
+++
@@ -1,4 +0,0 @@
-const coreConfig = {
-    EXPRESS_URL: 'https://fakturas.kevinposada.es/api/',
-}
-export default coreConfig
